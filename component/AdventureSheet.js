1e19a6be94c28c56ead7e861e2f7de66ce5a4dfc
Ondřej Kratochvíl <ondrakratochvil@seznam.cz>
2023-04-29 20:16:54 +0200

    Modify attributes

--- component/AdventureSheet.js
+++ component/AdventureSheet.js
@@ -4,26 +4,36 @@ import { useState } from 'react';
 import { messages } from '../constants/localization';
 
 export default function AdventureSheet() {
-    const [skill, setSkill] = useState(6);
-    const [stamina, setStamina] = useState(12);
-    const [luck, setLuck] = useState(6);
+    const [initialSkill, setInitialSkill] = useState(6);
+    const [initialStamina, setInitialStamina] = useState(12);
+    const [initialLuck, setInitialLuck] = useState(6);
+    const [skill, setSkill] = useState(initialSkill);
+    const [stamina, setStamina] = useState(initialStamina);
+    const [luck, setLuck] = useState(initialLuck);
     return (
         <View>
             <Text style={styles.title}>{messages.adventureSheet}</Text>
             <View>
-                <View>
+                <Text>{messages.skill}</Text>
+                <View style={styles.container}>
                     {changeAttribute(skill, setSkill, -1)}
-                    <Text>{messages.skill}: {skill}</Text>
+                    <Text style={styles.skillValue}>{skill}/{initialSkill}</Text>
                     {changeAttribute(skill, setSkill, 1)}
                 </View>
-                <View>
+            </View>
+            <View>
+                <Text>{messages.stamina}</Text>
+                <View style={styles.container}>
                     {changeAttribute(stamina, setStamina, -1)}
-                    <Text>{messages.stamina}: {stamina}</Text>
+                    <Text style={styles.skillValue}>{stamina}/{initialStamina}</Text>
                     {changeAttribute(stamina, setStamina, 1)}
                 </View>
-                <View>
+            </View>
+            <View>
+                <Text>{messages.luck}</Text>
+                <View style={styles.container}>
                     {changeAttribute(luck, setLuck, -1)}
-                    <Text>{messages.luck}: {luck}</Text>
+                    <Text style={styles.skillValue}>{luck}/{initialLuck}</Text>
                     {changeAttribute(luck, setLuck, 1)}
                 </View>
             </View>
@@ -46,4 +56,14 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
     },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    skillValue: {
+        paddingHorizontal: '3px',
+        fontWeight: 'bold',
+    }
 });
